f40b45c23a7dc02873207efefb98f10d942097c5
Athul Bhandary <athulbhandary28@gmail.com>
2023-11-21 18:21:17 +0530

    Project detail

--- src/files/prog.java
+++ src/files/prog.java
@@ -4,5 +4,6 @@ public class prog {
     public static void main(String[] args) {
         System.out.println("Car Rental System- OOPs Project");
         System.out.println("This is Athul");
+        System.out.println("This is my OOPS project");
     }
 }
